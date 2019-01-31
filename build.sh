0817bdcb84c306acd44fdab44851006de3046e9a
Juan Zuloaga <jzuloaga@csc.conicet.gov.ar>
2019-01-31 14:35:25 -0300

    first

---
+++ build.sh
@@ -0,0 +1,1 @@
+docker build -t xvan/powerdevs:r981 -f image/Dockerfile .
---
+++ run.sh
@@ -0,0 +1,14 @@
+docker run -it \
+    --workdir=$HOME \
+    --user=$(id -u):$(id -g) \
+    --env="HOME" \
+    --env="DISPLAY" \
+    --volume="/etc/group:/etc/group:ro" \
+    --volume="/etc/passwd:/etc/passwd:ro" \
+    --volume="/etc/shadow:/etc/shadow:ro" \
+    --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume=/home/$USER/dockerhome:/$HOME \
+    --volume=$XAUTHORITY:/home/$USER/.Xauthority \
+    xvan/powerdevs:r981 \
+    /opt/powerdevs/bin/run.sh
